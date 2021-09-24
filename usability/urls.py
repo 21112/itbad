e8a42da8b479429c39324d59b8befc91b69d0dc2
I'Go <73431874+21112@users.noreply.github.com>
2021-09-24 23:54:27 +0200

    Delete usability directory

--- usability/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.usability_home, name='usabiliti-home'),
-    path('what-is-usability', views.description_usability, name='description-usability')
-    
-]
